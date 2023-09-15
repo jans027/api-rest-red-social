ab650b2c7a6eb5e4cf4e9365d3e09dd98b22d7da
jans027 <jans027@gmail.com>
2023-09-15 16:02:46 -0500

    creando conexion a base de datos

---
+++ index.js
@@ -0,0 +1,18 @@
+// importar dependencias
+const connection = require("./database/connection");
+
+//  Mensaje de bienvenida
+console.log("API NODE para RED SOCIAL arrancada !!")
+
+// Conexion a base de datos
+connection();
+
+// Crear servidor node
+
+// Configurar cors
+
+// Convertir los datos del body a objetos js
+
+// Cargar y configurar rutas 
+
+// poner servidor a escuchar peticiones http
