845512eba5904732d086b42dc79ca2faf393e433
jans027 <jans027@gmail.com>
2023-09-15 16:13:34 -0500

    creando ruta de prueba

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // importar dependencias
 const connection = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
 
 //  Mensaje de bienvenida
 console.log("API NODE para RED SOCIAL arrancada !!")
@@ -8,11 +10,31 @@ console.log("API NODE para RED SOCIAL arrancada !!")
 connection();
 
 // Crear servidor node
+const app = express();
+const puerto = 3900;
 
 // Configurar cors
+app.use(cors());
 
 // Convertir los datos del body a objetos js
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 // Cargar y configurar rutas 
 
+// Ruta de prueba
+app.get("/ruta-prueba", (req, res) =>{
+
+    return res.status(200).json(
+        {
+            "id":1,
+            "nombre":"Julian",
+            "web":"jansdesarrolladorweb.com"
+        }
+    )
+})
+
 // poner servidor a escuchar peticiones http
+app.listen(puerto, () => {
+    console.log(`Servidor de node corriendo en el puerto ${puerto}`)
+})
