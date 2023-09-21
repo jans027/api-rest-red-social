981bfff0198b35463d60d36639243cbb06d025c7
jans027 <jans027@gmail.com>
2023-09-21 10:52:33 -0500

    estructurando y creando carpeta publicaciones en bd

--- controllers/publications.js
+++ controllers/publications.js
@@ -1,13 +1,80 @@
-
+const Publication = require("../models/publications")
 
 // Acciones de prueba
-const pruebaPublication = (req, res) =>{
+const pruebaPublication = (req, res) => {
     return res.status(200).send({
+        status: "success",
         message: "Mensaje enviado desde: controller/publication.js"
-    })
+    });
 }
+
+// Guardar publicacion
+const save = (req, res) => {
+
+    // Recoger datos del body
+    const params = req.body;
+
+    // Si no llegan dar respuesta negativa
+    if (!params) {
+
+        return res.status(400).send({
+            status: "error",
+            message: "Debes enviar el texto de la publicacion."
+        })
+    }
+
+    // Crear y rellenar el objeto del modelo
+    let newPublication = new Publication(params);
+    newPublication.user = req.user.id;
+
+    // Guardar objeto en bd
+    newPublication.save()
+        .then((publicationStored) => {
+
+            if (!publicationStored) {
+                return res.status(400).send({
+                    status: "error",
+                    message: "No se ha guardado la publicacion."
+                });
+            };
+
+            return res.status(200).send({
+                status: "success",
+                message: "Publicacion guardada",
+                publicationStored
+            });
+
+        }).catch((err) => {
+
+            return res.status(500).send({
+                status: "error",
+                message: "Error guardando la publicacion."
+            });
+        });
+
+};
+// Sacar una publicacion en concreto
+const detail = (req, res) => {
+
+    return res.status(200).send({
+        status: "success",
+        message: "Metodo mostrar publicacion"
+    });
+
+};
+
+// Eliminar publicaciones
+
+// listar todas las publicaciones (de usuarios que sigo)
+
+// Listar publicaciones de un usuario
+
+// Subir ficheros
+
+// Devolver archivos multimedia (imagenes)
 
 // Exportar acciones
 module.exports = {
-    pruebaPublication
+    pruebaPublication,
+    save
 }
--- routes/publications.js
+++ routes/publications.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const PublicationController = require("../controllers/publications");
+const check = require("../middlewares/auth");
 
 // Definir rutas
 router.get("/prueba-publication", PublicationController.pruebaPublication);
+
+// Ruta util
+router.post("/save", check.auth, PublicationController.save);
+
 
 // Exportar router
 module.exports = router
